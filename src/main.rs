e65667b2756c8efc224c9c69c9647c45bd0c4125
Andrew Cowie <istathar@gmail.com>
2024-08-05 11:40:14 +1000

    Incorporate syntect::easy example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::BufRead;
+use syntect::easy::HighlightFile;
+use syntect::highlighting::{Style, ThemeSet};
+use syntect::parsing::SyntaxSet;
+use syntect::util::as_24_bit_terminal_escaped;
+
 fn main() {
-    println!("Hello, world!");
+    let ss = SyntaxSet::load_from_folder(".").unwrap();
+    let ts = ThemeSet::load_defaults();
+
+    let mut highlighter = HighlightFile::new(
+        "Example.t",
+        &ss,
+        &ts.themes["Solarized (dark)"],
+    ).unwrap();
+    for maybe_line in highlighter.reader.lines() {
+        let line = maybe_line.unwrap();
+        let regions: Vec<(Style, &str)> = highlighter
+            .highlight_lines
+            .highlight_line(&line, &ss).unwrap();
+        println!("{}", as_24_bit_terminal_escaped(&regions[..], true));
+    }
 }
