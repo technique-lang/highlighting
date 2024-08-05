e8907c6bc62726f4061fc2b725af8002864e817c
Andrew Cowie <istathar@gmail.com>
2024-08-05 20:08:09 +1000

    Print scope for each parsed section

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 use std::io::BufRead;
 use syntect::easy::HighlightFile;
 use syntect::highlighting::{Style, ThemeSet};
-use syntect::parsing::SyntaxSet;
+use syntect::parsing::{ParseState, ScopeStack, SyntaxSet};
 use syntect::util::as_24_bit_terminal_escaped;
 
 fn main() {
     let ss = SyntaxSet::load_from_folder(".").unwrap();
     let ts = ThemeSet::load_defaults();
 
-    let mut highlighter = HighlightFile::new(
-        "Example.t",
-        &ss,
-        &ts.themes["Solarized (dark)"],
-    ).unwrap();
-    for maybe_line in highlighter.reader.lines() {
-        let line = maybe_line.unwrap();
-        let regions: Vec<(Style, &str)> = highlighter
-            .highlight_lines
-            .highlight_line(&line, &ss).unwrap();
-        println!("{}", as_24_bit_terminal_escaped(&regions[..], true));
+    let syn = ss.find_syntax_by_name("Technique").unwrap();
+
+    let mut parser = ParseState::new(syn);
+
+    let input = "hello : World -> Planet";
+
+    let result = parser.parse_line(input, &ss).unwrap();
+
+    // println!("{:?}", result);
+
+    let mut stack = ScopeStack::new();
+    // println!("{:?}", stack);
+
+    let mut prev = 0;
+
+    for (next, op) in result {
+        // println!("{:?} {:?}", next, op);
+
+        stack.apply(&op).unwrap();
+
+        let scope = stack.scopes.last().unwrap();
+
+        println!("{}", scope.build_string());
+
+        println!("{}", &input[prev..next]);
+        prev = next;
     }
 }
