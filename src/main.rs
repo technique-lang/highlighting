f42b93823bca651fd8509e1fcbcd7e98426bd3d4
Andrew Cowie <istathar@gmail.com>
2024-08-06 12:08:19 +1000

    Refactor to use anyhow crate

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
+use anyhow::{Context, Result};
 use syntect::highlighting::ThemeSet;
 use syntect::parsing::{ParseState, Scope, ScopeStack, SyntaxSet};
 
-fn main() {
-    let ss = SyntaxSet::load_from_folder(".").unwrap();
+fn main() -> Result<()> {
+    let ss = SyntaxSet::load_from_folder(".")?;
     let ts = ThemeSet::load_defaults();
 
-    let syn = ss.find_syntax_by_name("Technique").unwrap();
+    let syn = ss
+        .find_syntax_by_name("Technique")
+        .context("Syntax for Technique not found")?;
 
     let mut parser = ParseState::new(syn);
 
     let input = "hello : World -> Planet";
 
-    let result = parser.parse_line(input, &ss).unwrap();
+    let result = parser.parse_line(input, &ss)?;
 
     let mut stack = ScopeStack::new();
 
     let mut prev = 0;
-    let mut current = Scope::new("").unwrap();
+    let mut current = Scope::new("")?;
 
     for (next, op) in result {
-        stack.apply(&op).unwrap();
+        stack.apply(&op)?;
 
-        let scope = stack.scopes.last().unwrap();
+        let scope = stack
+            .scopes
+            .last()
+            .expect("No scope on stack?!?");
 
         println!("{}", current.build_string());
 
@@ -30,4 +36,5 @@ fn main() {
 
         current = *scope;
     }
+    Ok(())
 }
