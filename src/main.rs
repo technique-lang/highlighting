f60a3611a8c24f7521d0c3d52839af4a90ef24cd
Andrew Cowie <istathar@gmail.com>
2024-08-08 22:58:05 +1000

    Syntax highlight text fragments associated with each scope

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{Context, Result};
-use syntect::highlighting::ThemeSet;
+use syntect::highlighting::{Highlighter, ThemeSet};
 use syntect::parsing::{ParseState, Scope, ScopeStack, SyntaxSet};
+use syntect::util::as_24_bit_terminal_escaped;
 
 fn main() -> Result<()> {
     let ss = SyntaxSet::load_from_folder(".")?;
@@ -29,7 +30,22 @@ fn main() -> Result<()> {
             .last()
             .expect("No scope on stack?!?");
 
-        println!("{:35.35} {}", current.build_string(), &input[prev..next]);
+        // retrieve the appropriate ANSI syntax highlighting for the scope
+
+        let theme = ts
+            .themes
+            .get("Solarized (dark)")
+            .expect("Theme not found");
+
+        let highlighter = Highlighter::new(theme);
+
+        let text = &input[prev..next];
+
+        let style = highlighter.style_for_stack(std::slice::from_ref(&current));
+
+        let output = as_24_bit_terminal_escaped(&[(style, text)], true);
+
+        println!("{:35.35} {}\x1b[0m", current.build_string(), output);
         prev = next;
 
         current = *scope;
